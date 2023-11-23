65bbebcec1a7fdfef4cb970d801ecda14b7f6127
lchzh3473 <lchzh3473@163.com>
2023-11-24 00:27:23 +0800

    v0.3.0

--- src/pec/index.ts
+++ src/pec/index.ts
@@ -1,9 +1,28 @@
 import { parse as parse$ } from './pec2json';
 import { parse as parseRPE$ } from './rpe2json';
 import { readInfo as readInfo$ } from './readInfo';
+import { normalizeLineEvents, normalizeSpeedEvents } from '../format';
 namespace PEC {
-  export const parse = parse$;
-  export const parseRPE = parseRPE$;
+  export function parse(pec: string, filename: string): ReturnType<typeof parse$> {
+    const result = parse$(pec, filename);
+    for (const line of result.data.judgeLineList) {
+      normalizeSpeedEvents(line.speedEvents);
+      normalizeLineEvents(line.judgeLineDisappearEvents);
+      normalizeLineEvents(line.judgeLineMoveEvents);
+      normalizeLineEvents(line.judgeLineRotateEvents);
+    }
+    return result;
+  }
+  export function parseRPE(pec: string, filename: string): ReturnType<typeof parseRPE$> {
+    const result = parseRPE$(pec, filename);
+    for (const line of result.data.judgeLineList) {
+      normalizeSpeedEvents(line.speedEvents);
+      normalizeLineEvents(line.judgeLineDisappearEvents);
+      normalizeLineEvents(line.judgeLineMoveEvents);
+      normalizeLineEvents(line.judgeLineRotateEvents);
+    }
+    return result;
+  }
   export const readInfo = readInfo$;
 }
 export default PEC;
